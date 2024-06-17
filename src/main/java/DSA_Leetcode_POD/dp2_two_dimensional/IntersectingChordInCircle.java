bcbe7ddf747236933ecf2e7284a10ec6c0b5e504
Avnish Kumar <88883963+acko-avnish@users.noreply.github.com>
2024-06-18 03:09:37 +0530

    Intersecting Chords in a Circle

--- src/main/java/DSA_Leetcode_POD/dp2_two_dimensional/IntersectingChordInCircle.java
+++ src/main/java/DSA_Leetcode_POD/dp2_two_dimensional/IntersectingChordInCircle.java
@@ -1,4 +1,23 @@
 package DSA_Leetcode_POD.dp2_two_dimensional;
 
+import java.util.Scanner;
+
 public class IntersectingChordInCircle {
+    public static int chordCnt(int A){
+        int mod = 1000000007;
+        int [] dp=new int[A+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=A;i++){
+            for(int j=0;j<i;j++){
+                dp[i]+=(dp[j]%mod*dp[i-1-j]%mod)%mod;
+            }
+        }
+        return dp[A];
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        System.out.println("number of ways you can draw A chords in a circle with 2 x A points such that no 2 chords intersect : "+chordCnt(A));
+    }
 }
