67513fadec038a9bf4c02acc56c9cc45940193e0
Avnish Kumar <88883963+acko-avnish@users.noreply.github.com>
2024-08-27 07:02:04 +0530

    latest code

--- src/main/java/DSA_quick_reacap/Trees/View_Types/Main.java
+++ src/main/java/DSA_quick_reacap/Trees/View_Types/Main.java
@@ -8,6 +8,7 @@ public class Main {
         ArrayList<Integer> arrayList=new ArrayList<>();
         for(int i=0;i<5;i++){
             arrayList.add(0,i);
+           // Integer.MAX_VALUE , Integer.MIN_VALUE
         }
         for(int i=0;i<arrayList.size();i++){
             System.out.print(arrayList.get(i)+" ");
