2469268d547e2e4794685db9c92f73d57b8e1938
Avnish <avnish.kumar@ip-192-168-0-159.ap-south-1.compute.internal>
2024-09-05 08:47:07 +0530

    design pattern 2 builder

---
+++ src/main/java/Design_Patter_DP_2_Builder/Main.java
@@ -0,0 +1,7 @@
+package Design_Patter_DP_2_Builder;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to world of Design Pattern Part 2 : Builder");
+    }
+}
